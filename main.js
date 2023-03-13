b1f676f494ffeacb096521bd068d289c88be811c
Sh0KingFE <kbednarczyk@interia.com>
2023-03-13 20:28:55 +0100

    boostrap web

---
+++ main.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2gwa2luZyIsImEiOiJjbGY1c2FkcHIwbzJ6NDBvNGNrMXJrbGJwIn0.cLWctQmB7G1Auak4nKupTA';
+  var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    center: [-71.060982, 42.35725],
+    zoom: 18,
+  });
